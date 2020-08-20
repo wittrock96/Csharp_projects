234e41e996fb068059a8d49e613ff3f93d1893a5
cole <cole.wittrock0@gmail.com>
2020-08-20 15:45:37 -0500

    added human class and classes project

---
+++ classes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Human Cole = new Human("cole");
+
+            Human Hunter = new Human("hunter");
+
+            Cole.attack(Hunter);
+
+
+
+        }
+    }
+
+    class Human
+    {
+        public String Name;
+        public int strength;        
+        public int intelligence;
+        public int dexterity;
+        private int health;
+
+        public Human(string val){
+            Name = val;
+            strength = 3;
+            intelligence = 3;
+            dexterity = 3;
+            health = 100;
+
+        }
+
+        public Human(string name, int setStrength, int setIntelligence, int setDexterity, int setHealth)
+        {
+            Name = name;
+            strength = setStrength;
+            intelligence = setIntelligence;
+            dexterity = setDexterity;
+            health = setHealth;
+
+        }
+
+        public int attack(Human Target){
+            Console.WriteLine($"{this.Name} attacks {Target.Name}");
+            Console.WriteLine(Target.health);
+
+            int damage = 5 * this.strength;
+            Target.health -= damage;
+
+            Console.WriteLine(Target.health);
+
+            return Target.health;
+
+        }
+
+
+
+    }
+}
---
+++ puzzles.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+namespace puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Names());
+            // RandomArray();
+
+            // string temp = TossCoin();
+            // Console.WriteLine(temp);
+            
+        }
+
+        public static int[] RandomArray(){
+            Random rand = new Random();
+
+            int[] randArray = new int[20];
+
+            for(int i = 0; i < randArray.Length; i++){
+                randArray[i] = rand.Next(5,25);
+                
+            }
+
+            return randArray;
+
+        }
+
+        public static String TossCoin(){
+            Console.WriteLine("tossing a coin");           
+
+            Random rand = new Random();
+
+            int temp = rand.Next(1,100);
+
+            if(temp < 50){
+                return "Heads!";
+            }
+            else{
+                return "Tails";
+            }           
+
+        }
+
+        public static List<string> Names(){
+            Random rand = new Random();
+            List<string> nameList = new List<string>();
+            List<string> newList = new List<string>();
+            
+            nameList.Add("Todd");
+            nameList.Add("Tiffany");
+            nameList.Add("Charlie");
+            nameList.Add("Geneva");
+            nameList.Add("Sydney");
+
+            int count = nameList.Count;
+            int last = count-1;
+
+            for(int i = 0; i < last; i ++){
+                int r = rand.Next(i, count);
+                var tmp = nameList[i];
+                nameList[i] = nameList[r];
+                nameList[r] = tmp;
+                Console.WriteLine(nameList[i]);
+                if (nameList[i].Length > 5){
+                    newList.Add(nameList[i]);
+                }
+            }
+
+            return newList;
+
+
+
+        }
+    }
+}
